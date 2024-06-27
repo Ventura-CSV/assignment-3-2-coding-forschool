abfeb65949776c15b417882b53d0a806839c98b9
Ethan Wang <ethan_wang2@my.vcccd.edu>
2024-06-26 17:37:34 -0700

    First

--- main.py
+++ main.py
@@ -2,13 +2,34 @@ def main():
     num1 = int(input('Enter the first number: '))
     num2 = int(input('Enter the second number: '))
     num3 = int(input('Enter the third number: '))
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
 
-    print(minval, median, maxval)
+    minval = 0
+    median = 0
+    maxval = 0 
+    
+    if num1 < num2 and num1 < num3:
+        minval = num1
+    elif num2 <num1 and num2 < num3:
+        minval = num2
+    else:
+        minval = num3
+    
+    if num1 > num2 and num1 < num3:
+        median = num1
+    elif num2> num1 and num2 < num3:
+        median = num2
+    else:
+        median = num3
+    
+    if num1 > num2 and num1 > num3:
+        maxval = num1
+    elif num2 > num1 and num2 > num3:
+        maxval = num2
+    else:
+        maxval = num3
+    
+    
+    print(minval, median, maxval) 
     ########################################
     # Do not delete the return statement
     ########################################
